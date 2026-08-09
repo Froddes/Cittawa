7fdd0125560ef01afdd9b52a7bb6bfaa08f80960
Frarodmar19 <frarodmar19@alum.us.es>
2026-08-09 14:17:35 +0200

    vite config

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        demo: resolve(__dirname, 'demo.html'),
+        privacidad: resolve(__dirname, 'privacidad.html'),
+        terminos: resolve(__dirname, 'terminos.html'),
+        aviso: resolve(__dirname, 'aviso.html'),
+      }
+    }
+  }
+})
